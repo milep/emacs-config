f387f52d91e43f4678044a11d37d4d906eb03906
Miika Leppänen <miika.leppanen@iki.fi>
2016-01-15 21:37:56 +0200

    web mode auto closing

--- init.el
+++ init.el
@@ -65,6 +65,7 @@
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
+  (setq web-mode-enable-auto-closing t)
 )
 (add-hook 'web-mode-hook  'my-web-mode-hook)
 
