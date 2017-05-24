493559dbbd609eaaa7694b549b092eb37b1cb7df
Miika Leppänen <miika.leppanen@iki.fi>
2017-05-24 08:28:34 +0300

    Org clock statusbar app

--- init.el
+++ init.el
@@ -107,3 +107,7 @@
 
 ;; Autosave bookmarks
 (setq bookmark-save-flag 1)
+
+;; https://github.com/koddo/org-clock-statusbar-app
+(add-hook 'org-clock-in-hook (lambda () (call-process "/usr/bin/osascript" nil 0 nil "-e" (concat "tell application \"org-clock-statusbar\" to clock in \"" (replace-regexp-in-string "\"" "\\\\\"" org-clock-current-task) "\""))))
+(add-hook 'org-clock-out-hook (lambda () (call-process "/usr/bin/osascript" nil 0 nil "-e" "tell application \"org-clock-statusbar\" to clock out")))
